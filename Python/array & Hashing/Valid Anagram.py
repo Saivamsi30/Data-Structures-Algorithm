a679d1a2b4d90f79f870a5dc7b5b6ce2755ce804
Saivamsi30 <saivamsi8239@gmail.com>
2025-09-07 10:04:18 +0530

    Leetcode 242

---
+++ Python/array & Hashing/Valid Anagram.py
@@ -0,0 +1,24 @@
+class Solution:
+    def ValidAnagram(self,s:str,t:str)->bool:
+        if len(s) != len(t):
+            return False
+        
+        countS,countT = {},{}
+
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i],0)
+            countT[t[i]] = 1 + countT.get(t[i],0)
+            
+        for c in countS:
+            if countS[c] != countT.get(c,0):
+                return False
+        return True
+    
+s = "racecar"
+t = "carrace"
+obj = Solution()
+print(obj.ValidAnagram(s,t))
+
+# s = "rat"
+# t = "cat"
+# o/p -> False
