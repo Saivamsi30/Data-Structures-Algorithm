49669d73eadd85117a74957df070a6b93d1547a1
Saivamsi30 <saivamsi8239@gmail.com>
2025-09-11 10:41:13 +0530

    Leetcode 125

---
+++ Two Pointers/Valid Palindrome.py
@@ -0,0 +1,24 @@
+class Solution:
+    def ispalindrome(self,s:int)->bool:
+        l = 0
+        r = len(s) - 1
+
+        while l < r:
+            while l<r and not alphaNum(s[l]):
+                l += 1
+            while r > l and not alphaNum(s[r]):
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            
+            l,r = l+1,r-1
+        return True
+    
+def alphaNum(c):
+    return (ord('A') <= ord(c) <= ord('Z') or
+            ord('a') <= ord(c) <= ord('z') or
+            ord('0') <= ord(c) <= ord('9'))
+
+s = "Was it a car or a cat I saw"
+obj = Solution()
+print(obj.ispalindrome(s))
